784a9322e1f28f34678afa23920341194547fd0d
skullY <skullydazed@gmail.com>
2016-05-28 21:37:56 -0700

    App refinement and progress towards a working windows installer
    * Added notes about what needs to happen on windows
    * Added support for electron-packager
    * Added support for drag and drop operations
    * Make the window a little bigger
    * Flash button only enabled when bootloader on mcu is responding
    * Removed the Reset button
    * Removed the non-native title bar and close button
    * Added the OS chrome (frame) back to the window
    * Use the status window to prompt the user for the next step
    * Cleanup inconsistent formatting

--- app.js
+++ app.js
@@ -5,25 +5,41 @@ var Menu = remote.require('menu');
 var dialog = remote.require('dialog');
 var sys = remote.require('sys');
 var exec = remote.require('child_process').exec;
+var bootloader_ready = false;
+var flash_in_progress = false;
 
-$(document).ready(function() {  
+$(document).ready(function() {
+  /* Handle drag-n-drop events
+   */
+  $(document).on('dragenter dragover', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+
+  $(document).on('drop', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var file = e.originalEvent.dataTransfer.files[0];
+    loadHex(file.path);
+  });
+
+  /* Bind actions to our buttons
+   */
   $('#flash-hex').attr('disabled','disabled');
-  sendStatus("Ready to work!\n");
   $('#close-window-button').bind('click', function (event) {
     window.close();
   });
   $('#load-file').bind('click', function (event) {
-    $("#file-path").val(loadFile());
-    enableButtons();
+    loadHex(loadFile());
   });
   $('#flash-hex').bind('click', function (event) {
     disableButtons();
     sendHex($("#file-path").val(), function(success) {
-      enableButtons();
       if (success) {
-        sendStatus("Flashing complete!\n");
+        sendStatus("Flashing complete!");
       } else {
-        sendStatus("An error occured - please try again.\n");
+        sendStatus("An error occured - please try again.");
       }
     });
   });
@@ -32,32 +48,63 @@ $(document).ready(function() {
     resetChip(function(success) {
       enableButtons();
       if (success) {
-        sendStatus("Reset complete!\n");
+        sendStatus("Reset complete!");
       } else {
-        sendStatus("An error occured - please try again.\n");
+        sendStatus("An error occured - please try again.");
       }
     });
   });
+
+  /* Ready to go
+   */
+  window.setTimeout(checkForBoard, 10);
+  sendStatus("Select a firmware file by clicking 'Choose .hex' or drag and drog a file onto this window.");
 });
+
+function loadHex(filename) {
+  /* Load a file and prepare to flash it.
+   */
+  if (filename.slice(-4) != '.hex') {
+    sendStatus("Invalid firmware file: " + filename);
+    return;
+  }
+
+  $("#file-path").val(filename);
+  enableButtons();
+  clearStatus();
+
+  if (!bootloader_ready) sendStatus("Press RESET on your keyboard's PCB.");
+}
 
 function disableButtons() {
   $('#flash-hex').attr('disabled','disabled');
-  $('#reset').attr('disabled','disabled');
+  $('#flash-hex').css('background-color', 'red');
+  $('#flash-hex').css('color', 'black');
 }
 
 function enableButtons() {
-  if ($('#file-path').val() != "")
+  if (bootloader_ready && $('#file-path').val() != "") {
     $('#flash-hex').removeAttr('disabled');
-  $('#reset').removeAttr('disabled');
+    $('#flash-hex').css('background-color', 'green');
+    $('#flash-hex').css('color', 'white');
+  }
 }
 
-function sendStatus(text) {
+function clearStatus() {
+  $('#status').text('');
+}
+
+function writeStatus(text) {
   $('#status').append(text);
   $('#status').scrollTop($('#status')[0].scrollHeight);
 }
 
+function sendStatus(text) {
+  writeStatus(text + "\n");
+}
+
 function loadFile() {
-  return dialog.showOpenDialog({ 
+  return dialog.showOpenDialog({
     properties: [ 'openFile' ],
     filters: [
       { name: 'Custom File Type', extensions: ['hex'] }
@@ -66,6 +113,7 @@ function loadFile() {
 }
 
 function sendHex(file, callback) {
+  flash_in_progress = true;
   eraseChip(function(success) {
     if (success) {
       // continue
@@ -90,6 +138,7 @@ function sendHex(file, callback) {
       callback(false);
     }
   });
+  flash_in_progress = false;
 };
 
 function dfu_folder() {
@@ -110,11 +159,11 @@ var escapeShell = function(cmd) {
 
 function eraseChip(callback) {
   var command = dfu_location() + " atmega32u4 erase --force";
-  sendStatus("<b>" +command + "</b>\n");
+  sendStatus("<b>" +command + "</b>");
   exec(command, {cwd: dfu_folder()},
     function(error, stdout, stderr) {
-    sendStatus(stdout);
-    sendStatus(stderr);
+    writeStatus(stdout);
+    writeStatus(stderr);
     if (stderr.indexOf("no device present") > -1) {
       callback(false);
     } else {
@@ -125,11 +174,11 @@ function eraseChip(callback) {
 
 function flashChip(file, callback) {
   var command = dfu_location() + " atmega32u4 flash " + file;
-  sendStatus("<b>" +command + "</b>\n");
+  sendStatus("<b>" +command + "</b>");
   exec(command, {cwd: dfu_folder()},
     function(error, stdout, stderr) {
-    sendStatus(stdout);
-    sendStatus(stderr);
+    writeStatus(stdout);
+    writeStatus(stderr);
     if (stderr.indexOf("no device present") > -1) {
       callback(false);
     } else {
@@ -140,15 +189,35 @@ function flashChip(file, callback) {
 
 function resetChip(callback) {
   var command = dfu_location() + " atmega32u4 reset";
-  sendStatus("<b>" +command + "</b>\n");
+  sendStatus("<b>" +command + "</b>");
   exec(command, {cwd: dfu_folder()},
     function(error, stdout, stderr) {
-    sendStatus(stdout);
-    sendStatus(stderr);
+    writeStatus(stdout);
+    writeStatus(stderr);
     if (stderr.indexOf("no device present") > -1) {
       callback(false);
     } else {
       callback(true);
     }
   });
+}
+
+function checkForBoard() {
+  if (flash_in_progress) {
+    window.setTimeout(checkForBoard, 10000);
+  } else {
+    var command = dfu_location() + " atmega32u4 get bootloader-version";
+    exec(command, {cwd: dfu_folder()}, function(error, stdout, stderr) {
+      if (stdout.indexOf("Bootloader Version:") > -1) {
+        if (!bootloader_ready && $('#file-path').val() != "") clearStatus();
+        bootloader_ready = true;
+        if ($('#file-path').val() != "") enableButtons();
+        window.setTimeout(checkForBoard, 10000);
+      } else {
+        bootloader_ready = false;
+        disableButtons();
+        window.setTimeout(checkForBoard, 1000);
+      }
+    });
+  }
 }
