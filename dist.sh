78811a27d910ada9b40f8423ab9a546aa478a494
akappel <atkappel@gmail.com>
2016-07-23 17:43:37 -0400

    Updated all references to old project name
    Changed all references to 'dfu-programmer-app' to 'qmk-firmware-flasher'
    where applicable.

--- dist.sh
+++ dist.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cp -R * /Users/j/Downloads/dfu-programmer-app-darwin/dfu-programmer.app/Contents/Resources/app/
-cp -R * /Users/j/Downloads/dfu-programmer-app-win32/resources/app/
-rm /Users/j/Downloads/dfu-programmer-app-darwin.zip
-rm /Users/j/Downloads/dfu-programmer-app-win32.zip
+cp -R * /Users/j/Downloads/qmk-firmware-flasher-darwin/qmk-firmware-flasher.app/Contents/Resources/app/
+cp -R * /Users/j/Downloads/qmk-firmware-flasher-win32/resources/app/
+rm /Users/j/Downloads/qmk-firmware-flasher-darwin.zip
+rm /Users/j/Downloads/qmk-firmware-flasher-win32.zip
 cd /Users/j/Downloads
-zip -r /Users/j/Downloads/dfu-programmer-app-darwin.zip dfu-programmer-app-darwin/ -x node_modules
-zip -r /Users/j/Downloads/dfu-programmer-app-win32.zip dfu-programmer-app-win32/ -x node_modules
+zip -r /Users/j/Downloads/qmk-firmware-flasher-darwin.zip qmk-firmware-flasher-darwin/ -x node_modules
+zip -r /Users/j/Downloads/qmk-firmware-flasher-win32.zip qmk-firmware-flasher-win32/ -x node_modules
