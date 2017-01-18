194030845cb552d86205b444a72ac114fdad77f6
NoahAndrews <noahandrews@users.noreply.github.com>
2017-01-18 00:42:51 -0500

    Examine program files

--- dist-win32.bat
+++ dist-win32.bat
@@ -11,7 +11,8 @@ set PACKAGE_DIR="%OUTPUT_DIR%\QMK Firmware Flasher-%PLATFORM%-%ARCH%"
 
 set WIX_DIR="C:\Program Files (x86)\WiX Toolset v3.10\bin"
 
-dir "C:\Program Files (x86)\WiX Toolset v3.8\bin"
+echo Program Files (x86) contents
+dir "C:\Program Files (x86)"
 
 call npm install
 
