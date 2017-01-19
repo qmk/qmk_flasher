afaa5b241e4bf5271b588055763386a245f627b2
skullY <skullydazed@gmail.com>
2017-01-18 20:19:50 -0800

    Update to match recent changes

--- src/main.js
+++ src/main.js
@@ -26,10 +26,10 @@ app.on('ready', function() {
   let mainWinOptions = {show: false, frame: true, resizable: false};
   if (process.platform == 'win32') {
     mainWinOptions.width = 659;
-    mainWinOptions.height = 510;
+    mainWinOptions.height = 260;
   } else {
     mainWinOptions.width = 640;
-    mainWinOptions.height = 480;
+    mainWinOptions.height = 230;
   }
 
   mainWin = new BrowserWindow(mainWinOptions);
