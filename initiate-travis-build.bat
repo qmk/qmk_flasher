54f284a9a5b4dd8346a7f13ee0778747f6b7ca52
NoahAndrews <noahandrews@users.noreply.github.com>
2017-01-20 03:09:16 -0500

    Make travis build the correct branch

--- initiate-travis-build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_TOKEN%" -d '{"request"={"branch":"master"}}' "https://api.travis-ci.org/repo/NoahAndrews%%2Fqmk_firmware_flasher/requests"
