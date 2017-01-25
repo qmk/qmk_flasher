02b3e2b2f52c437b6f57686cc200c37fc5983637
NoahAndrews <noahandrews@users.noreply.github.com>
2017-01-25 13:35:44 -0500

    Prepare for uploading to Bintray

---
+++ packaging/update-bintray-version.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const bintrayJsonPath = path.resolve(__dirname, "bintray-travis.json");
+
+let bintrayJson = fs.readFileSync(bintrayJsonPath, "utf8");
+
+let bintrayObj = JSON.parse(bintrayJson);
+
+bintrayObj.version.name = `v${process.env.npm_package_version}`;
+
+fs.writeFileSync(bintrayJsonPath, JSON.stringify(bintrayObj, null, 2));
