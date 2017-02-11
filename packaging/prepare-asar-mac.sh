1274816017d751aeca5df9b5c92f99fd6113d386
NoahAndrews <noahandrews@users.noreply.github.com>
2017-02-11 12:27:09 -0500

    Fix prepare-asar-mac.sh

--- packaging/prepare-asar-mac.sh
+++ packaging/prepare-asar-mac.sh
@@ -3,4 +3,4 @@
 PARENTDIR=`dirname "$0"`
 
 cd ${PARENTDIR}/../dist/mac
-cp QMK Firmware Flasher.app/Contents/Resources/app.asar mac-${npm_package_version}.asar
+cp "QMK Firmware Flasher.app/Contents/Resources/app.asar" ./mac-${npm_package_version}.asar
